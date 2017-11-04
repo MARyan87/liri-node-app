12bc2c5c7e72d9190545d42ecd875f07d1537f89
unknown <jpek919@yahoo.com>
2017-11-03 20:50:44 -0400

    final

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '926554484616122369-POj286gtSSIR7kNBqCaJAITNF6e3nf2',
   access_token_secret: 'p15qzIBZgwUkYvt73SQO8tPDTjLPgBDPLk8DmN4q0in2l',
 }
+
+exports.spotifyKeys = {
+  client_id: '6d07e01e4a9441ad90e2b4deee3e77f7',
+  client_secret: '5e06a076b0a24595a9c238491181ff49',
+}
--- liri.js
+++ liri.js
@@ -1,55 +1,32 @@
 var fs = require('fs');
+var request = require('request');
+var keys = require('./keys.js');
 var Twitter = require('twitter');
 var spotify = require('spotify');
 var omdb = require('omdb');
-var request = require('request');
 var input1 = process.argv[2];
 var input2 = process.argv.splice(3).join(" ");
 
-function log() {
+liri();
 
-    fs.appendFile('./log.txt', input1 + " " + input2 + ", ", function(err) {
-
-        // If an error was experienced we say it.
-        if (err) {
-            console.log(err);
-        }
-
-        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
-        else {
-            // console.log("Content Added!");
-        }
-
-    });
-};
-
-var keys = require('./keys.js');
-// console.log(keys.twitterKeys);
+function liri() {
 
 var client = new Twitter(keys.twitterKeys);
-
 var params = {
     screen_name: 'LiriLirison',
     count: 20
 };
 
-run();
-
-function run() {
-
     if (input1 === "my-tweets") {
-
         client.get('statuses/user_timeline', params, function(error, tweets, response) {
             if (!error) {
                 console.log('');
                 console.log('My Last 20 Tweets: ');
                 console.log('--------------------------');
                 tweets.forEach(function(individualTweet) {
-                    console.log('Time Posted: ' + individualTweet.created_at);
-                    console.log('Tweet: ' + individualTweet.text);
-                    console.log('--------------------------');
-
-
+                console.log('Time Posted: ' + individualTweet.created_at);
+                console.log('Tweet: ' + individualTweet.text);
+                console.log('--------------------------');
                 });
 
             } else {
@@ -57,20 +34,17 @@ function run() {
             };
         });
 
-        log();
 
     } else if (input1 === "spotify-this-song") {
-
         if (input2.length < 1) {
-
-            input2 = "The Sign Ace of Base";
+            input2 = "Ace of Base The Sign";
         };
 
-        spotify.search({ type: 'track', query: input2 }, function(err, data) {
-            if (err) {
-                console.log('Error occurred: ' + err);
-                return;
-            }
+    spotify.search({ type: 'track', query: input2 }, function(err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
             console.log('');
             console.log('Spotify Song Information Results: ');
             console.log('--------------------------');
@@ -81,23 +55,15 @@ function run() {
             console.log('--------------------------');
         });
 
-        log();
 
     } else if (input1 === "movie-this") {
-
         if (input2.length < 1) {
-
-            input2 = "Mr. Nobody";
+                input2 = "Mr. Nobody";
         };
 
-        // Then run a request to the OMDB API with the movie specified
         request("http://www.omdbapi.com/?t=" + input2 + "&y=&plot=short&r=json&tomatoes=true", function(error, response, body) {
-
-            // If the request is successful (i.e. if the response status code is 200)
             if (!error && response.statusCode === 200) {
 
-                // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-                // console.log(JSON.parse(body));
                 console.log('');
                 console.log('OMDB Movie Information: ');
                 console.log('--------------------------');
@@ -114,16 +80,11 @@ function run() {
             } else {
 
                 console.log(error);
-
             }
-
         });
 
-        log();
 
     } else if (input1 === "do-what-it-says") {
-
-        log();
 
         fs.readFile('random.txt', 'utf8', function(err, data) {
             if (err) throw err;
@@ -133,9 +94,7 @@ function run() {
 
             input1 = arr[0].trim();
             input2 = arr[1].trim();
-            run();
-
+            liri();
         });
-
     }
 };
