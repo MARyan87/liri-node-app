e7e1a26e8ac962487244d18e4a3900a313e88029
unknown <jpek919@yahoo.com>
2017-11-03 20:00:04 -0400

    push

---
+++ keys.js
@@ -0,0 +1,8 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'ZtmxWEgyHZiKBHKZHTkiMG508',
+  consumer_secret: 'GoZYheeeOopCpJt1TYN25gOp8u440dsGImmzR9mGbHBya81top',
+  access_token_key: '926554484616122369-POj286gtSSIR7kNBqCaJAITNF6e3nf2',
+  access_token_secret: 'p15qzIBZgwUkYvt73SQO8tPDTjLPgBDPLk8DmN4q0in2l',
+}
